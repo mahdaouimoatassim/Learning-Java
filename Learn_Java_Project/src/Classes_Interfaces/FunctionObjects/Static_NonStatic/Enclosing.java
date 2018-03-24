d56619336192f905b33b4ac648602380d6da0f9a
Mahdaoui ElMoutassim Billah <ce_mahdaoui@esi.dz>
2018-03-24 16:00:32 +0100

    Ajouter Item 22

---
+++ Learn_Java_Project/src/Classes_Interfaces/FunctionObjects/Static_NonStatic/Enclosing.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes_Interfaces.FunctionObjects.Static_NonStatic;
+
+/**
+ *
+ * @author El Mouatassim Billah
+ */
+public class Enclosing {
+    
+    int h=3;
+    public class Nested{
+    
+        int k=5;
+    
+    } 
+    
+    
+}
