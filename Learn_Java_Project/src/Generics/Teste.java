2c24440d642ef344ae305350130d24a92ca70079
Mahdaoui ElMoutassim Billah <ce_mahdaoui@esi.dz>
2018-03-28 20:34:53 +0100

    Ajouter l'item 24

--- Learn_Java_Project/src/Generics/Teste.java
+++ Learn_Java_Project/src/Generics/Teste.java
@@ -12,33 +12,24 @@ import java.util.*;
  * @author El Mouatassim Billah
  */
 public class Teste {
-  public static void main(String[] args) {
-  
-      
-      /**
-       * Teste du Item 23 RawType And Generics Types.
-       */
-//        List<RawType_Stamp> Stamps = new ArrayList<RawType_Stamp>();
-//      RawType_Stamp Stamp1 = new RawType_Stamp("Name1", "Description1");
-//      RawType_Stamp Stamp2 = new RawType_Stamp("Name2", "Description2");
-//      RawType_Stamp Stamp3 = new RawType_Stamp("Name3", "Description3");
-//      Stamps.addAll(Arrays.asList(Stamp1, Stamp2, Stamp3));
-//      RawType_Stamp Stamp4 = null;
-//      for (RawType_Stamp s : Stamps) {
-//          System.out.println(s.getName() + "," + s.getDescription());
-//      }
-//
-//      for (Iterator<RawType_Stamp> i = Stamps.iterator(); i.hasNext();) {
-//          Stamp4 = i.next();
-//          System.out.println(Stamp4.getName() + "," + Stamp4.getDescription());
-//      }
-//      
-      
-//      List<String> strings = new ArrayList<String>();
-//        unsafeAdd(strings ,new String(" srb"));
-//        String s = strings.get(0); 
-  }
-    
+    /**
+     * Methode de teste pour le Item 24 eliminate unchecked Error.
+     * @param <T>
+     * @param a
+     * @return 
+     */
+    public static <T> T[] toArray(T[] a) {
+          int size=16;
+        if (a.length < size) {
+         @SuppressWarnings("unchecked") T[] result = (T[]) Arrays.copyOf(a, size, a.getClass());
+         return result;
+        }
+        System.arraycopy(a, 0, a, 0, size);
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+    }
     private static void unsafeAdd(List  list, Object o) {
         list.add(o);
     }
@@ -74,5 +65,45 @@ public class Teste {
         }
     }
     
+    
+    
+  public static void main(String[] args) {
+      /**
+       * Teste du Item 23 RawType And Generics Types.
+       */
+//        List<RawType_Stamp> Stamps = new ArrayList<RawType_Stamp>();
+//      RawType_Stamp Stamp1 = new RawType_Stamp("Name1", "Description1");
+//      RawType_Stamp Stamp2 = new RawType_Stamp("Name2", "Description2");
+//      RawType_Stamp Stamp3 = new RawType_Stamp("Name3", "Description3");
+//      Stamps.addAll(Arrays.asList(Stamp1, Stamp2, Stamp3));
+//      RawType_Stamp Stamp4 = null;
+//      for (RawType_Stamp s : Stamps) {
+//          System.out.println(s.getName() + "," + s.getDescription());
+//      }
+//
+//      for (Iterator<RawType_Stamp> i = Stamps.iterator(); i.hasNext();) {
+//          Stamp4 = i.next();
+//          System.out.println(Stamp4.getName() + "," + Stamp4.getDescription());
+//      }
+//
+//      List<String> strings = new ArrayList<String>();
+//        unsafeAdd(strings ,new String(" srb"));
+//        String s = strings.get(0);
+      
+
+      
+      
+      Set<RawType_Stamp> exaltation = new HashSet();
+      
+      RawType_Stamp[] tableau1={new RawType_Stamp("1","1") ,new RawType_Stamp("2","2"),new RawType_Stamp("3","3"),new RawType_Stamp("4","4")};
+      RawType_Stamp[] tableau2=null;
+      
+      tableau2= toArray(tableau1);
+
+      System.out.println(tableau2[1].getName());
+  }
+  
+  
+      
     
 }
